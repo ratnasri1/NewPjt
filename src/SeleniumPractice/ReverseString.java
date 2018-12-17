83abfb9826d80b2e4e5a94c194eaa7b644d8c3f4
tm <tm@tm-6>
2018-12-17 11:30:33 +0530

    javaprogram

---
+++ src/SeleniumPractice/ReverseString.java
@@ -0,0 +1,28 @@
+package SeleniumPractice;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+	
+	  
+	        String input = "ratna sri"; 
+	  
+	        StringBuilder input1 = new StringBuilder(); 
+	  
+	        // append a string into StringBuilder input1 
+	        input1.append(input); 
+	  
+	        // reverse StringBuilder input1 
+	        input1 = input1.reverse(); 
+	  
+	        // print reversed String 
+	        System.out.println(input1); 
+
+	}
+
+	
+	
+	
+	
+
+}
